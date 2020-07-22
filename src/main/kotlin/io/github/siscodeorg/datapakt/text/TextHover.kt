ca5d0f88ab47ed3333437e70be41f05acce2193b
wffirilat <xwffirilat@gmail.com>
2020-07-21 22:47:45 -0500

    Collapse TextHoverAction.contents hierarchy by one level

--- src/main/kotlin/io/github/siscodeorg/datapakt/text/TextHover.kt
+++ src/main/kotlin/io/github/siscodeorg/datapakt/text/TextHover.kt
@@ -2,29 +2,21 @@ package io.github.siscodeorg.datapakt.text
 
 import com.beust.klaxon.Json
 
-
 sealed class TextHoverAction(
     @Json(index = 1)
     val action: String,
     @Json(index = 2)
-    val contents: HoverShowContents
+    val contents: Any
 ) { }
 
-sealed class HoverShowContents
 
-data class ShowTextContents(var show_text: TextComponent) : HoverShowContents()
+data class ShowTextContents(var show_text: TextComponent)
 
 class PlaintextTextHoverAction(
-    show_text: TextComponent
-) : TextHoverAction("show_text",
-contents = ShowTextContents(show_text)
-)
+    contents: TextComponent
+) : TextHoverAction("show_text", contents)
 
 data class ShowItemContents(
-    val show_item: ShowItemInfo
-) : HoverShowContents()
-
-data class ShowItemInfo(
     var id: String,  // really a namespaced id (ResourceLocation)
     var count: Int = 1,
     var tag: String? = null  // really an SNBT value
@@ -35,12 +27,10 @@ class ItemHoverAction(
     count: Int = 1,
     tag: String? = null
 ) : TextHoverAction("show_item",
-    contents = ShowItemContents(ShowItemInfo(id, count, tag))
+    contents = ShowItemContents(id, count, tag)
 )
 
-data class ShowEntityContents(val show_entity: ShowEntityInfo) : HoverShowContents()
-
-data class ShowEntityInfo(
+data class ShowEntityContents(
     var type: String, // actually a ResourceLocation
     var id: String,  // actually a UUID
     var name: TextComponent? = null
@@ -52,6 +42,6 @@ class EntityHoverAction(
     name: TextComponent? = null
 ) : TextHoverAction(
     "show_entity",
-    contents = ShowEntityContents(ShowEntityInfo(type, id, name))
+    contents = ShowEntityContents(type, id, name)
 )
 
