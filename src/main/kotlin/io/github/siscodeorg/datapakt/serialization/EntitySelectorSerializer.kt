cf96e810971c28ba7a1d555061615b8eece3292c
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-09-03 23:23:19 -0300

    Add EntitySelectorSerializer.kt

---
+++ src/main/kotlin/io/github/siscodeorg/datapakt/serialization/EntitySelectorSerializer.kt
@@ -0,0 +1,25 @@
+package io.github.siscodeorg.datapakt.serialization
+
+import com.google.gson.*
+import io.github.siscodeorg.datapakt.entity.*
+import java.lang.UnsupportedOperationException
+import java.lang.reflect.Type
+
+class EntitySelectorSerializer : JsonSerializer<EntitySelector> {
+    override fun serialize(src: EntitySelector?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        val currentSelector : String
+        currentSelector = when(src?.type){
+            SelectorType.NEAREST_PLAYER -> "@p"
+            SelectorType.RANDOM_PLAYER -> "@r"
+            SelectorType.ALL_PLAYERS -> "@a"
+            SelectorType.ALL_ENTITIES -> "@e"
+            SelectorType.ENTITY_EXECUTING_COMMAND -> "@s"
+            null -> throw UnsupportedOperationException("Invalid/Not initialized Selector Type")
+        }
+        return if(src.arguments?.isEmpty()!!) JsonPrimitive(currentSelector)
+        else {
+            var serializedArgs = src.arguments?.map { it.serialize() }
+            JsonPrimitive(currentSelector+serializedArgs?.joinToString (",","[","]"))
+        }
+    }
+}
