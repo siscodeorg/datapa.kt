23e94225743751cd3be5008f68fe1755330032c6
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-09-03 23:22:46 -0300

    Add EntitySelector.kt

---
+++ src/main/kotlin/io/github/siscodeorg/datapakt/entity/EntitySelector.kt
@@ -0,0 +1,37 @@
+package io.github.siscodeorg.datapakt.entity
+
+enum class SelectorType {
+    NEAREST_PLAYER,RANDOM_PLAYER,ALL_PLAYERS,ALL_ENTITIES,ENTITY_EXECUTING_COMMAND
+}
+
+class EntitySelector {
+    var type: SelectorType? = null
+    var arguments: List<SelectorArgument>? = null
+}
+
+sealed class SelectorArgument {
+    abstract fun serialize(): String
+}
+
+class CoordinateArgument(var x : String, var y: String, var z: String) : SelectorArgument()
+{
+    override fun serialize() : String { return "x=${x},y=${y},z=${z}" }
+}
+
+//Distance is actually an Int (12) or Range (12..14)
+//So String was chosen until a better implementation is found
+class DistanceArgument(var distance: String) : SelectorArgument()
+{
+    override fun serialize(): String { return "distance=${distance}" }
+}
+
+class VolumeDimensionArgument (var dx : String, var dy: String, var dz: String) : SelectorArgument()
+{
+    override fun serialize(): String { return "dx=${dx},dy=${dy},dz=${dz}" }
+}
+
+//TODO: Implement Scores Argument
+//class ScoresArgument : SelectorArgument() { //scores={Score1=12,Score2=12..33}
+//
+//}
+
